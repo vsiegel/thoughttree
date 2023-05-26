a01c24b0fce53e95bfbdbc165cd6fb2175650b05
Volker Siegel <mail@volker-siegel.de>
2023-05-26 19:16:50 +0200

    import

--- thoughttree/thoughttree.py
+++ thoughttree/thoughttree.py
@@ -3,7 +3,7 @@ import os
 import tkinter as tk
 from argparse import Namespace
 from datetime import datetime
-from tkinter import ttk
+from tkinter import ttk, simpledialog
 from tkinter import font as tkfont
 from tkinter.messagebox import showinfo
 

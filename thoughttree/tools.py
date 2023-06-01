7be450503667df11684a6d997c7b8c0b365b35c3
Volker Siegel <mail@volker-siegel.de>
2023-06-01 12:35:09 +0200

    Move functions to tools fixup.

--- thoughttree/tools.py
+++ thoughttree/tools.py
@@ -12,3 +12,24 @@ def create_dummy_data(tree):
                     grandchild_key = f"{child_key}_G{g}"
                     tree.insert(child_key, "end", grandchild_key, text=grandchild_key, values=(g,))
                     tree.item(grandchild_key, tags='closed')
+
+
+def list_all_bindings(root):
+    bindings = {}
+    for child in root.winfo_children():
+        widget_bindings = child.bind()
+        if widget_bindings:
+            bindings[child] = widget_bindings
+        if child.winfo_children():
+            bindings.update(list_all_bindings(child))
+    return bindings
+
+
+def add_bboxes(bbox1, bbox2):
+    x1, y1, w1, h1 = bbox1
+    x2, y2, w2, h2 = bbox2
+    x = min(x1, x2)
+    y = min(y1, y2)
+    w = max(x1 + w1, x2 + w2) - x
+    h = max(y1 + h1, y2 + h2) - y
+    return x, y, w, h

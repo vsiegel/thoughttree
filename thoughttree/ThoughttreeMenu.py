17b07fa8dfe917052a426e3ee977496ca69f3356
Volker Siegel <mail@volker-siegel.de>
2023-06-03 01:06:43 +0200

    Root reference no longer needed, because Tk is ingerited

--- thoughttree/ThoughttreeMenu.py
+++ thoughttree/ThoughttreeMenu.py
@@ -11,16 +11,15 @@ from tools import list_all_bindings, add_bboxes
 
 class ThoughttreeMenu(Menu):
     def __init__(self, thoughttree, new_window_callback):
-        super().__init__(thoughttree.root, tearoff=0)
+        super().__init__(thoughttree, tearoff=0)
         self.new_window_callback = new_window_callback
         self.tt = thoughttree
-        self.root = thoughttree.root
         self.create_menu()
 
 
     @property
     def focus(self) -> Text:
-        return self.root.focus_get()
+        return self.tt.focus_get()
 
 
     def create_menu(self):
@@ -35,20 +34,19 @@ class ThoughttreeMenu(Menu):
 
         def save_chat(e=None):
             name = save(ChatFileManager.save_chat_dialog, "Chat saved to ")
-            self.root.title(name)
+            self.tt.title(name)
 
         def save_section(e=None):
             save(ChatFileManager.save_section_dialog, "Section saved to ")
 
         def save_code_block(e=None):
-            print("scb")
             save(ChatFileManager.save_code_block_dialog, "Code block saved to ")
 
         def new_window(event=None) :
             self.new_window_callback()
 
         def show_context_menu(event, menu) :
-            widget = self.root.winfo_containing(event.x_root, event.y_root)
+            widget = self.tt.winfo_containing(event.x_root, event.y_root)
             if widget :
                 widget.focus_set()
             menu.tk_popup(event.x_root, event.y_root)
@@ -98,9 +96,8 @@ class ThoughttreeMenu(Menu):
             self.focus.insert(tk.INSERT, f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
         def debug_info(event=None):
-
-            # print(f"{self.focus.bind_class('Text')=}")
-            print(f"{list_all_bindings(self.root)=}")
+            # print(f"{self.tt.event_info() =}")
+            # print(f"{list_all_bindings(self.tt)=}")
 
             bbox_start = self.focus.bbox('1.0')
             bbox_end = self.focus.bbox("end - 1 char")
@@ -153,7 +150,6 @@ class ThoughttreeMenu(Menu):
                 txt.configure(width=int(parent_width / char_width))
 
 
-            # Bind the <Configure> event to the on_resize function
             self.focus.bind('<Configure>', on_resize)
 
             txt.bind('<<Modified>>', on_text_change)
@@ -162,7 +158,8 @@ class ThoughttreeMenu(Menu):
         def item(label, keystroke, command, bind_key=True, context_menu=None):
             menu.item(label, keystroke, command, bind_key, context_menu)
 
-        bar = Menu(self.tt.root)
+        # bar = Menu(self.tt)
+        bar = self
 
         menu = Menu(bar, "File")
         item("New Window", "<Control-n>", new_window)
@@ -193,8 +190,8 @@ class ThoughttreeMenu(Menu):
         item("Increase Font Size", "<Control-plus>", lambda e: change_text_size(1))
         item("Decrease Font Size", "<Control-minus>", lambda e: change_text_size(-1))
         item("Reset Font Size", "<Control-period>", lambda e: change_text_size(0))
-        self.root.bind("<Control-Button-4>", lambda event: change_text_size(1))
-        self.root.bind("<Control-Button-5>", lambda event: change_text_size(-1))
+        self.tt.bind("<Control-Button-4>", lambda event: change_text_size(1))
+        self.tt.bind("<Control-Button-5>", lambda event: change_text_size(-1))
         oldmenu = menu
         menu = Menu(oldmenu, "Wrap")
         item("Char", None, lambda e: wrap(tk.CHAR))
@@ -217,9 +214,9 @@ class ThoughttreeMenu(Menu):
         item("Complete 5 Times", "<Control-Key-5>", lambda e: self.tt.chat_continue("", "", 5))
         item("Complete Multiple...", "<Control-m>", lambda e: self.tt.chat_continue("", "", 0))
         item("Complete Multiple Again", "<Alt-Return>", lambda e: self.tt.chat_continue("", "", -1))
-        item("Cancel", "<Escape>", self.tt.gpt.cancel)
+        item("Cancel", "<Escape>", self.tt.cancelModels)
         menu.add_separator()
-        for model_name in self.tt.gpt.get_available_models() :
+        for model_name in self.tt.model.get_available_models() :
             if model_name == "gpt-4":
                 key = "<Control-Alt-Key-4>"
             elif model_name == "gpt-3.5-turbo":
@@ -233,4 +230,4 @@ class ThoughttreeMenu(Menu):
         item("Debug Info", "<Control-i>", debug_info)
         item("About", None, None)
 
-        self.root.bind_class("Text", "<Button-3>", lambda event: show_context_menu(event, edit_menu))
+        self.tt.bind_class("Text", "<Button-3>", lambda event: show_context_menu(event, edit_menu))

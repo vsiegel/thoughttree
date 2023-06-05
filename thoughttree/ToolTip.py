b36fd3e5e1ce3029337bc592ec08782f999c8472
Volker Siegel <mail@volker-siegel.de>
2023-06-05 20:05:21 +0200

    Renamed vars

--- thoughttree/ToolTip.py
+++ thoughttree/ToolTip.py
@@ -5,31 +5,31 @@ class ToolTip :
     def __init__(self, widget, text) :
         self.widget = widget
         self.text = text
-        self.tooltip_window = None
+        self.tooltip = None
         self.widget.bind("<Enter>", self.show_tooltip)
         self.widget.bind("<Leave>", self.hide_tooltip)
 
 
     def show_tooltip(self, event=None) :
-        if self.tooltip_window is not None :
+        if self.tooltip is not None :
             return
 
         x, y, _, _ = self.widget.bbox("insert")
         x += self.widget.winfo_rootx() + 25
         y += self.widget.winfo_rooty() + 25
 
-        self.tooltip_window = tw = tk.Toplevel(self.widget)
-        tw.wm_overrideredirect(True)
-        tw.wm_geometry(f"+{x}+{y}")
+        self.tooltip = tooltip = tk.Toplevel(self.widget)
+        tooltip.wm_overrideredirect(True)
+        tooltip.wm_geometry(f"+{x}+{y}")
 
-        label = tk.Label(tw, text=self.text, background="#FFFFE0", relief="solid",
+        label = tk.Label(tooltip, text=self.text, background="#FFFFE0", relief="solid",
                 borderwidth=1, justify=tk.LEFT, padx=4, pady=4)
         label.pack()
 
 
     def hide_tooltip(self, event=None) :
-        if self.tooltip_window is None :
+        if self.tooltip is None :
             return
 
-        self.tooltip_window.destroy()
-        self.tooltip_window = None
+        self.tooltip.destroy()
+        self.tooltip = None

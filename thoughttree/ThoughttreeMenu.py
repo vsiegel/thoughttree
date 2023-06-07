780827859b8da94da892f79e79e4bb2f1b61eef1
Volker Siegel <mail@volker-siegel.de>
2023-06-07 21:23:50 +0200

    Renamed menu entry

--- thoughttree/ThoughttreeMenu.py
+++ thoughttree/ThoughttreeMenu.py
@@ -194,7 +194,7 @@ class ThoughttreeMenu(Menu):
         item("Calculate Cost", "", None)
 
         menu = Menu(self, "Navigate")
-        item("Branch Conversation", "<Control-b>", lambda e: self.focussed.split_conversation())
+        item("Split Conversation", "<Control-b>", lambda e: self.focussed.split_conversation())
         item("Jump to Similar Line", "<Control-j>", self.tt.jump_to_similar_line)
 
         menu = Menu(self, "Chat")

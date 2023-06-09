ee40120f7271c4c588a443aca4e29c1e615ae2fa
Volker Siegel <mail@volker-siegel.de>
2023-06-09 23:05:45 +0200

    New file with file type mappings

---
+++ thoughttree/file-types.py
@@ -0,0 +1,22 @@
+code_block_ext = {
+    # C-like languages
+    "c": ".c", "cpp": ".cpp", "csharp": ".cs", "java": ".java", "objective-c": ".m", "scala": ".scala",
+
+    # Scripting languages
+    "python": ".py", "javascript": ".js", "ruby": ".rb", "php": ".php", "perl": ".pl", "lua": ".lua",
+    "groovy": ".groovy", "r": ".r", "coffeescript": ".coffee", "elixir": ".ex", "erlang": ".erl",
+    "haskell": ".hs", "julia": ".jl", "matlab": ".m", "pascal": ".pas", "vimscript": ".vim",
+
+    # Mobile and systems languages
+    "swift": ".swift", "go": ".go", "rust": ".rs", "kotlin": ".kt", "dart": ".dart", "fsharp": ".fs",
+
+    # Markup and data formats
+    "html": ".html", "css": ".css", "xml": ".xml", "json": ".json", "markdown": ".md", "tex": ".tex", "latex": ".tex",
+    "yaml": ".yml", "ini": ".ini", "toml": ".toml",
+
+    # Shell and scripting
+    "bash": ".sh", "powershell": ".ps1", "shell": ".sh", "typescript": ".ts", "raku": ".raku",
+
+    # Other languages and technologies
+    "sql": ".sql", "elm": ".elm", "dockerfile": "Dockerfile", "dart": ".dart", "tex": ".tex", "latex": ".tex",
+}

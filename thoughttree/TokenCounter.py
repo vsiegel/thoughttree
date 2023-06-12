bd52b53be22aa01fb58cd1e09481a9a4be44321e
Volker Siegel <mail@volker-siegel.de>
2023-06-12 20:55:44 +0200

    TokenCounter simplified Formating of summary output. Removed draft code Main implementation of TokenCounter

--- thoughttree/TokenCounter.py
+++ thoughttree/TokenCounter.py
@@ -33,32 +33,79 @@ MODEL_DATA = {
 
 
 class TokenCounter:
-    def __init__(self, model_name, parent=None):
-        if parent:
-            self.prompt___tokens_sum = parent.prompt___tokens_sum
-            self.complete_tokens_sum = parent.complete_tokens_sum
-        else:
-            self.prompt___tokens_sum = 0
-            self.complete_tokens_sum = 0
+    def __init__(self, model_name):
+        self.model_name = model_name
 
-        def start(self):
-            self.span_start = TokenCounter(self)
+        self.prompt_tokens_total = 0
+        self.prompt_tokens_at_go = 0
+        self.completion_tokens_total = 0
+        self.completion_tokens_at_go = 0
 
-        def since_start(self):
-            pass
+    def go(self):
+        self.prompt_tokens_at_go = self.prompt_tokens_total
+        self.completion_tokens_at_go = self.completion_tokens_total
 
 
+    def prompt_tokens_since_go(self):
+        return self.prompt_tokens_total - self.prompt_tokens_at_go
 
-if __name__ == "__main__":
-    token_count = TokenCounter("a model")
-    #
-    #
-    #
-    token_count.start()
-    #
-    #
-    token_count.count_prompt(history)
-    #
-    #
-    #
-    token_count.count_complete(text)
+    def completion_tokens_since_go(self):
+        return self.completion_tokens_total - self.completion_tokens_at_go
+
+    def tokens_since_go(self):
+        return self.prompt_tokens_since_go() + self.completion_tokens_since_go()
+
+    def tokens_total(self):
+        return self.prompt_tokens_total + self.completion_tokens_total
+
+
+    def prompt_cost_since_go(self):
+        return self.prompt_cost(self.prompt_tokens_since_go())
+
+    def completion_cost_since_go(self):
+        return self.completion_cost(self.completion_tokens_since_go())
+
+    def prompt_cost_total(self):
+        return self.prompt_cost(self.prompt_tokens_total)
+
+    def completion_cost_total(self):
+        return self.completion_cost(self.completion_tokens_total)
+
+    def cost_since_go(self):
+        return self.prompt_cost_since_go() + self.completion_cost_since_go()
+
+    def cost_total(self):
+        return self.prompt_cost(self.prompt_tokens_total) + self.completion_cost(self.completion_tokens_total)
+
+
+    def count_tokens(self, text):
+        enc = tiktoken.encoding_for_model(self.model_name)
+        return len(enc.encode(text))
+
+    def observe_prompt(self, history):
+        for item in history:
+            text = item['content']
+            self.prompt_tokens_total += self.count_tokens(text)
+
+    def observe_completion(self, text):
+        self.completion_tokens_total += self.count_tokens(text)
+
+    def prompt_cost(self, tokens):
+        return (tokens * MODEL_DATA[self.model_name]['1k_token_usd']['prompt']) / 1000.0
+
+    def completion_cost(self, tokens):
+        return (tokens * MODEL_DATA[self.model_name]['1k_token_usd']['completion']) / 1000.0
+
+    def summary_since_go(self):
+        return f"{self.model_name} query:   {self.prompt_tokens_since_go():6d}+{self.completion_tokens_since_go():6d}={self.tokens_since_go():6d}t "\
+               + f"{self.prompt_cost_since_go():.5f}+{self.completion_cost_since_go():.5f}={self.cost_since_go():.5f}$"
+
+    def summary_total(self):
+        return f"{self.model_name} session: {self.prompt_tokens_total:6d}+{self.completion_tokens_total:6d}={self.tokens_total():6d}t "\
+               + f"{self.prompt_cost_total():.5f}+{self.completion_cost_total():.5f}={self.cost_total():.5f}$"
+
+    def summary(self):
+        return self.summary_since_go() + "\n" + self.summary_total()
+
+    def summarize(self):
+        print(self.summary())

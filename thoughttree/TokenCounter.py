508cdb801b332d7cb6e953179d82b6aa03e4d886
Volker Siegel <mail@volker-siegel.de>
2023-06-12 20:51:40 +0200

    Added model data and sources

--- thoughttree/TokenCounter.py
+++ thoughttree/TokenCounter.py
@@ -1,3 +1,37 @@
+import tiktoken
+
+
+# Sources:
+# https://platform.openai.com/docs/models/gpt-4
+# https://platform.openai.com/docs/models/gpt-3-5
+# https://openai.com/pricing
+# as of 2023-06-09
+
+MODEL_DATA = {
+    'gpt-3.5-turbo': {
+        '1k_token_usd': {
+            'prompt': 0.002,
+            'completion': 0.002,
+        },
+        'max_tokens': 4096,
+    },
+    'gpt-4': {
+        '1k_token_usd': {
+            'prompt': 0.03,
+            'completion': 0.06,
+        },
+        'max_tokens': 8192,
+    },
+    'gpt-4-32k': {
+        '1k_token_usd': {
+            'prompt': 0.06,
+            'completion': 0.12,
+        },
+        'max_tokens': 32768,
+    },
+}
+
+
 class TokenCounter:
     def __init__(self, model_name, parent=None):
         if parent:

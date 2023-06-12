20c25356fab74ec5b4e2e356fc46a7b1473f9e1e
Volker Siegel <mail@volker-siegel.de>
2023-06-12 11:50:37 +0200

    Moved MODEL_DATA to reduce (cyclic) dependency

--- thoughttree/Model.py
+++ thoughttree/Model.py
@@ -11,35 +11,6 @@ import openai
 import History
 
 
-# Sources:
-# https://platform.openai.com/docs/models/gpt-4
-# https://platform.openai.com/docs/models/gpt-3-5
-# https://openai.com/pricing
-# as of 2023-06-09
-
-MODEL_DATA = {
-    'gpt-3.5-turbo': {
-        '1k_token_usd': {
-            'prompt': 0.002,
-            'completion': 0.002,
-        },
-        'max_tokens': 4096,
-    },
-    'gpt-4': {
-        '1k_token_usd': {
-            'prompt': 0.03,
-            'completion': 0.06,
-        },
-        'max_tokens': 8192,
-    },
-    'gpt-4-32k': {
-        '1k_token_usd': {
-            'prompt': 0.06,
-            'completion': 0.12,
-        },
-        'max_tokens': 32768,
-    },
-}
 
 class Model:
 

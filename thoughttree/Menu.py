53a8a2a524c82e3ced863fd11381873514bc1e2f
Volker Siegel <mail@volker-siegel.de>
2023-06-26 13:13:57 +0200

    Make use of MenuHelpTooltip Simplify name

--- thoughttree/Menu.py
+++ thoughttree/Menu.py
@@ -2,15 +2,26 @@ import re
 import tkinter as tk
 from typing import Union
 
+from MenuHelpTooltip import MenuHelpTooltip
+
 
 class Menu(tk.Menu):
 
-    def __init__(self, master: Union[tk.Tk, tk.Text, tk.Menu], label=None, **kwargs) :
+    def __init__(self, master: Union[tk.Tk, tk.Text, tk.Menu], label=None, menu_help=None, **kwargs) : #1
         super().__init__(master, tearoff=False, borderwidth=3)
-        if label :
-            master.add_cascade(label=label, menu=self, underline=0)
-        if type(master) == tk.Tk:
+        if menu_help:
+            self.menu_help = menu_help
+        else:
+            if isinstance(master, Menu):
+                self.menu_help = master.menu_help
+            else:
+                self.menu_help = None
+        if isinstance(master, tk.Tk):
             master.config(menu=self)
+        else:
+            master.add_cascade(label=label, menu=self, underline=0)
+            if self.menu_help:
+                MenuHelpTooltip(self, self.menu_help)
 
 
     def convert_key_string(self, keystroke) :

e9dc3c326a5af43416fdefd33a73500a5787d6ed
Volker Siegel <mail@volker-siegel.de>
2023-07-03 23:02:23 +0200

    New class

---
+++ thoughttree/FinishReasonIcon.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+from Text import Text
+from Tooltip import Tooltip
+
+
+class FinishReasonIcon(tk.Label):
+    def __init__(self, text: Text, symbol: str, tooltip=""):
+        super().__init__(text, text=symbol, padx=7, pady=0, bg="#F0F0F0", fg="grey", borderwidth=0)
+        if tooltip:
+            Tooltip(self, tooltip)

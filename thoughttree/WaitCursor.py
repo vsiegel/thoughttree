d46df4c6465c564d7f08ef98260e00daed2b30b3
Volker Siegel <mail@volker-siegel.de>
2023-07-04 02:13:10 +0200

    Save cursor

--- thoughttree/WaitCursor.py
+++ thoughttree/WaitCursor.py
@@ -1,14 +1,16 @@
 class WaitCursor():
     def __init__(self, widget):
         self.widget = widget
+        self.saved_cursor = None
 
     def __enter__(self):
+        self.saved_cursor = self.widget.cget('cursor')
         self.widget.config(cursor='watch')
         self.widget.update()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         try:
-            self.widget.config(cursor='')
+            self.widget.config(cursor=self.saved_cursor)
             self.widget.update()
         except:
             pass

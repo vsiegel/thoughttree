d2afdc67ee535586bac5e83e8d974c13e5269656
Volker Siegel <mail@volker-siegel.de>
2023-07-04 07:15:41 +0200

    temperature_label

--- thoughttree/StatusBar.py
+++ thoughttree/StatusBar.py
@@ -20,10 +20,13 @@ class StatusBar(tk.Frame):
         self.max_token_label = tk.Label(self, **defaults, width=10, text="(max_token)", anchor=W)
         self.max_token_label.pack(side=LEFT, padx=(5, 0), fill=X, expand=True)
 
-        self.model_label = tk.Label(self, **defaults, width=20, text=model_text, anchor=tk.E)
-        self.model_label.pack(side=tk.LEFT, padx=(5, 0), fill=tk.X)
+        self.temperature_label = tk.Label(self, **defaults, width=10, text="(max_token)", anchor=W)
+        self.temperature_label.pack(side=LEFT, padx=(5, 0), fill=X, expand=True)
 
-        self.pack(side=tk.BOTTOM, fill=tk.X)
+        self.model_label = tk.Label(self, **defaults, width=20, text=model_text, anchor=E)
+        self.model_label.pack(side=LEFT, padx=(5, 0), fill=X)
+
+        self.pack(side=BOTTOM, fill=X)
 
 
     def set_max_token_var(self, var: IntVar):

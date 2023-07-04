f43d7ef2b594860889af203a5565ba6520575fc7
Volker Siegel <mail@volker-siegel.de>
2023-07-04 16:10:20 +0200

    Less padding

--- thoughttree/FinishReasonIcon.py
+++ thoughttree/FinishReasonIcon.py
@@ -6,6 +6,6 @@ from Tooltip import Tooltip
 
 class FinishReasonIcon(tk.Label):
     def __init__(self, text: Text, symbol: str, tooltip=""):
-        super().__init__(text, text=symbol, padx=7, pady=0, bg="#F0F0F0", fg="grey", borderwidth=0)
+        super().__init__(text, text=symbol, padx=6, pady=0, bg="#F0F0F0", fg="grey", borderwidth=0)
         if tooltip:
             Tooltip(self, tooltip).delay_ms = 100

96a696c0897c0237500312aaac0c1886f4a6cb1a
Volker Siegel <mail@volker-siegel.de>
2023-07-12 22:07:52 +0200

    More defaults

--- thoughttree/Notebook.py
+++ thoughttree/Notebook.py
@@ -1,7 +1,10 @@
 from tkinter import ttk
 
+ttk.Style().layout("NoBorder.TNotebook", [])
+
+
 class Notebook(ttk.Notebook):
-    def __init__(self, parent, **kw):
-        super().__init__(parent, **kw)
+    def __init__(self, parent, style="NoBorder.TNotebook", takefocus=False, **kw):
+        super().__init__(parent, style=style, takefocus=takefocus, **kw)
         self.enable_traversal()
 

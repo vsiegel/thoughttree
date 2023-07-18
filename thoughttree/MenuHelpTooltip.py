a8fa665dc7693897a8028451bab022be85b2987a
Volker Siegel <mail@volker-siegel.de>
2023-07-18 07:03:43 +0200

    Display previous text on menu separator Remove what was factored out

--- thoughttree/MenuHelpTooltip.py
+++ thoughttree/MenuHelpTooltip.py
@@ -10,24 +10,18 @@ class MenuHelpTooltip(Tooltip):
         self.previous_missing_item = None
 
 
-    def refresh_tooltip(self, event=None):
-        if event:
-            self.last_y = event.y
-        if self.tooltip:
-            px = self.root.winfo_pointerx() + 75
-            py = self.root.winfo_pointery() + 25
-            self.tooltip.wm_geometry(f"+{px}+{py}")
-
-            try:
-                menu_item = self.widget.entrycget(f'@{self.last_y}', 'label')
-                if menu_item in self.menu_help:
-                    help_text = self.menu_help[menu_item]
-                    text = text_block(help_text)
-                else:
-                    text = menu_item
-                    if menu_item and self.previous_missing_item != menu_item:
-                        self.previous_missing_item = menu_item
-                self.label.configure(text=text)
-            except Exception as ex: # Menu separators have no "label"
-                self.label.configure(text="-")
-
+    def refresh_tooltip_text(self):
+        try:
+            menu_item = self.widget.entrycget(f'@{self.last_y}', 'label')
+            # print(f"{menu_item=}")
+            if menu_item in self.menu_help:
+                help_text = self.menu_help[menu_item]
+                text = text_block(help_text)
+            else:
+                text = menu_item
+                if menu_item and self.previous_missing_item != menu_item:
+                    self.previous_missing_item = menu_item
+            self.label.configure(text=text)
+        except Exception as ex: # Menu separators have no "label"
+            pass # leave text unchanged
+            # self.label.configure(text="-")

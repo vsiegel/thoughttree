e2c6e40e5c2520da391429d80157be415e528788
Volker Siegel <mail@volker-siegel.de>
2023-07-24 15:35:47 +0200

    Set console height

--- thoughttree/Console.py
+++ thoughttree/Console.py
@@ -5,9 +5,9 @@ from tkinter import END
 
 
 class Console(tk.scrolledtext.ScrolledText, io.TextIOBase):
-    def __init__(self, master, width=100, height=1000, **kw):
-        tk.scrolledtext.ScrolledText.__init__(self, master, undo=True, wrap=tk.WORD, width=width, height=height, takefocus=False,
-                                              font=("monospace", 8), **kw)
+    def __init__(self, master, width=100, height=5, **kw):
+        tk.scrolledtext.ScrolledText.__init__(self, master, undo=True, wrap=tk.WORD, width=width, height=height,
+            takefocus=False, font=("monospace", 8), **kw)
         io.TextIOBase.__init__(self)
         self.master = master
         self.vbar.config(width=18, takefocus=False, borderwidth=2)

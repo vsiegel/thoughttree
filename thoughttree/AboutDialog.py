351f1c4aa6d5e1fbcd0a1c643afd880ae7e85e6c
Volker Siegel <mail@volker-siegel.de>
2023-07-24 15:28:38 +0200

    Complete AboutDialog

--- thoughttree/AboutDialog.py
+++ thoughttree/AboutDialog.py
@@ -1,23 +1,24 @@
 import tkinter as tk
-import subprocess
+import tkinter.commondialog
 
 from Sheet import Sheet
 from ThoughttreeConfig import conf
 
+class AboutDialog(tk.Toplevel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-class AboutDialog:
-    def __init__(self, parent):
-        self.parent = parent
-        self.dialog = tk.Toplevel(parent)
-        self.dialog.title("About Thoughttree")
+        self.focus_set()
+        self.grab_set()
+        self.transient(parent)
+        self.title("About Thoughttree")
 
         self.git_version = conf.git_describe_version
 
-        tk.Label(self.dialog, font=Sheet.FONT, text="Thoughttree").pack(padx=8, pady=12)
-        tk.Label(self.dialog, font=Sheet.FONT, text=self.git_version).pack(padx=16, pady=2)
+        tk.Label(self, font=Sheet.FONT, text="About Thoughttree").pack(padx=8, pady=12)
+        tk.Label(self, font=Sheet.FONT, text=self.git_version).pack(padx=16, pady=2)
 
-        tk.Button(self.dialog, text="OK", command=self.dialog.destroy).pack(padx=8, pady=12)
-
+        tk.Button(self, text="OK", command=self.destroy).pack(padx=8, pady=12)
 
 
 # testing the AboutDialog

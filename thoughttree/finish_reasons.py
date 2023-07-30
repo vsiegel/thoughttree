b9bc779f69d46dcd26531cf778abe52a15bb571c
Volker Siegel <mail@volker-siegel.de>
2023-07-30 13:23:37 +0200

    Separated data from model, only remotely associated anyway

---
+++ thoughttree/finish_reasons.py
@@ -0,0 +1,6 @@
+finish_reasons = {
+    "stop": {"symbol": "", "tooltip": ""},
+    "length": {"symbol": "…", "tooltip": "The completion reached max_tokens tokens. It can be continued."},
+    "canceled": {"symbol": "☒", "tooltip": "The completion was canceled."},
+    "error": {"symbol": "⚠", "tooltip": "An error occurred while processing the completion."},
+}

93c42a4646c534d341b8812a197638db192cc3e5
Volker Siegel <mail@volker-siegel.de>
2023-08-03 20:56:42 +0200

    Set command for model menu keys

--- thoughttree/ModelsMenu.py
+++ thoughttree/ModelsMenu.py
@@ -21,16 +21,23 @@ class ModelsMenu(Menu):
 
 
     def load_available_models(self, event=None):
+        def on_eventA(event):
+            print("A" + str(event))
+
         if self.fixed_model_menu_items == -1:
             self.fixed_model_menu_items = self.index(tk.END) + 1
         present_items = self.index(tk.END) + 1
         if present_items > self.fixed_model_menu_items:
             self.delete(0, present_items - self.fixed_model_menu_items - 1)
         for i, model_name in enumerate(self.ui.model.get_available_models()):
+            key = None
             if model_name == "gpt-4":
                 key = "<Control-Alt-Key-4>"
             elif model_name == "gpt-3.5-turbo":
                 key = "<Control-Alt-Key-3>"
+
+            if key:
+                command = lambda e, model=model_name: self.selected_model.set(model)
             else:
-                key = None
-            self.item(model_name, key, None, index=i, variable=self.selected_model)
+                command = None
+            self.item(model_name, key, command, index=i, variable=self.selected_model)

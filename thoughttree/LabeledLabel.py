03384ce2d5d953f24c9fff8468320ac19d1ffc93
Volker Siegel <mail@volker-siegel.de>
2023-08-03 09:14:31 +0200

    input validation - incomplete

--- thoughttree/LabeledLabel.py
+++ thoughttree/LabeledLabel.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 class LabeledLabel(tk.Frame):
-    def __init__(self, master, label_text=None, entry_width=3, textvariable=None, *args, **kwargs):
+    def __init__(self, master, label_text=None, entry_width=3, textvariable=None, validatecommand=None, *args, **kwargs):
         super().__init__(master, *args, **kwargs, bg="light blue")
 
         self.textvariable = textvariable
@@ -10,5 +10,5 @@ class LabeledLabel(tk.Frame):
         self.label.pack(side="left")
 
         self.entry = tk.Entry(self, width=entry_width, # state="readonly", takefocus=False,
-                              textvariable=self.textvariable, bd=0, highlightthickness=0)
+                              textvariable=self.textvariable, bd=0, highlightthickness=0, validatecommand=validatecommand)
         self.entry.pack(side="left")

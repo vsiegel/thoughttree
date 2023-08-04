686b253ae41204cabaa6d73ce96e3b59e34b86d3
Volker Siegel <mail@volker-siegel.de>
2023-08-04 10:09:00 +0200

    File for future class, contains only the prompt for now,

---
+++ thoughttree/GeneratedTitle.py
@@ -0,0 +1,18 @@
+
+
+TITLE_GENERATION = '''\
+A title for this conversation, about 50 characters. Style does not matter,
+it is about the information.
+Ignore the system prompt.
+Do not refer to this prompt or to the title.
+It is used as a one line title for this conversation.
+Use the language of the conversation.
+If there is no chat history, the title will be the text Thoughttree only, that is important.
+Do not use the name otherwise.
+Do not use a prefix like "Title:".
+Output the unquoted text of the title, nothing else.
+'''
+
+#Output: unquoted text of the title, without any prefixes or comments:
+#Do not refer to the content of the system prompt.
+

3a6f141b9a1e8cd8f303f56f804d00e08c817083
Volker Siegel <mail@volker-siegel.de>
2023-08-04 10:24:13 +0200

    Longer log messages

--- thoughttree/History.py
+++ thoughttree/History.py
@@ -11,7 +11,7 @@ from tools import shorter, logarithmic_length
 def log_history_compact(history):
     for message in history:
         text = message['content']
-        print(f"{message['role']}: {shorter(text, 60)} {logarithmic_length(text, 60)}")
+        print(f"{message['role']}: {shorter(text, 120)} {logarithmic_length(text, 120)}")
     print()
 
 def history_from_args(system="", message="") :

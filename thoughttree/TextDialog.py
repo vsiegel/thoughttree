214f50922fd8625df28399e63e9cb602f1b6b1f3
Volker Siegel <mail@volker-siegel.de>
2023-08-04 10:09:42 +0200

    Replacing error dialog to allow resiting and copying

---
+++ thoughttree/TextDialog.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import ACTIVE, LEFT, BOTH
+from tkinter.simpledialog import Dialog
+
+
+class TextDialog(tk.simpledialog.Dialog):
+    def __init__(self, message=None, title=None, parent=None):
+        self.message = message
+        super().__init__(parent, title)
+
+    def body(self, parent):
+        background = self.cget("background")
+        text = tk.Text(parent, height=10, width=30, borderwidth=0, highlightthickness=0, background=background, font=("sans-serif", 11))
+        text.insert(tk.END, self.message)
+        text.config(state=tk.DISABLED)
+        text.pack(fill=BOTH, expand=True)
+        return text
+
+    def buttonbox(self):
+
+        box = tk.Frame(self)
+
+        w = tk.Button(box, text="OK", width=10, command=self.ok, default=ACTIVE)
+        w.pack(side=LEFT, padx=5, pady=5)
+        self.pack_slaves()[0].pack(fill=BOTH, expand=True, padx=15, pady=15) # hack to repack dialog content to expand
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
+

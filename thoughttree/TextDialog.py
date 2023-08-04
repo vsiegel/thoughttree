edd506768f40f1665515aac3148f64ef0a546bf4
Volker Siegel <mail@volker-siegel.de>
2023-08-04 10:12:16 +0200

    Adding todos

--- thoughttree/TextDialog.py
+++ thoughttree/TextDialog.py
@@ -7,10 +7,13 @@ class TextDialog(tk.simpledialog.Dialog):
     def __init__(self, message=None, title=None, parent=None):
         self.message = message
         super().__init__(parent, title)
+        # todo: Control-A for select all.
+        # todo: context menu with "Copy Message"
 
     def body(self, parent):
         background = self.cget("background")
-        text = tk.Text(parent, height=10, width=30, borderwidth=0, highlightthickness=0, background=background, font=("sans-serif", 11))
+        text = tk.Text(parent, height=10, width=30, borderwidth=0,
+                       highlightthickness=0, background=background, font=("sans-serif", 11))
         text.insert(tk.END, self.message)
         text.config(state=tk.DISABLED)
         text.pack(fill=BOTH, expand=True)

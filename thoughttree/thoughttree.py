777cd60c00a02c4d77e6ed8984a97ea4f87233b8
Volker Siegel <mail@volker-siegel.de>
2023-08-05 22:15:10 +0200

    Debug log

--- thoughttree/thoughttree.py
+++ thoughttree/thoughttree.py
@@ -144,6 +144,7 @@ class Thoughttree(UI):
 
 
     def update_window_title(self, event=None):
+        print(f"{id(self)}")
         progress_title = self.root.title() + "..."
 
         def write_title(content):

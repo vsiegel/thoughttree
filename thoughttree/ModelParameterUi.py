58f0a287859cbfd8163fe6539e9102459dd74c30
Volker Siegel <mail@volker-siegel.de>
2023-08-05 02:55:50 +0200

    New class

---
+++ thoughttree/ModelParameterUi.py
@@ -0,0 +1,34 @@
+from tkinter import SUNKEN
+
+from LabeledLabel import LabeledLabel
+
+
+class ModelParameterUi():
+    def __init__(self):
+        self.defaults = {"bd": 1, "relief": SUNKEN}
+
+        pass
+
+    def get_parameter_editor(self):
+        return None
+
+class TopPParameterUi(ModelParameterUi):
+    def __init__(self):
+        super().__init__()
+
+    def validate(self, entry_value):
+        try:
+            value = float(entry_value)
+            if 0 <= value <= 2:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+    def get_parameter_editor(self):
+        self.temperature_label = LabeledLabel(self, "Temp.:", entry_width=3, validatecommand=self.validate, **self.defaults)
+
+
+
+

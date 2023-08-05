b081e9a75362e42a13c1aab75cc878ed9d5f7bfc
Volker Siegel <mail@volker-siegel.de>
2023-08-05 22:11:48 +0200

    Renamed

--- thoughttree/Ui.py
+++ thoughttree/Ui.py
@@ -4,7 +4,7 @@ from os.path import join
 from tkinter import messagebox
 
 
-class UI(tk.Frame):
+class Ui(tk.Frame):
     icon = None
     WINDOW_ICON = None
     current_open_uis = []
@@ -19,8 +19,8 @@ class UI(tk.Frame):
         tk.Frame.__init__(self, self.root)
 
         self.is_root_destroyed = False
-        UI.current_open_uis.append(self)
-        name = f"{name} {len(UI.current_open_uis)}"
+        Ui.current_open_uis.append(self)
+        name = f"{name} {len(Ui.current_open_uis)}"
         self.root.title(name)
         self.root.wm_title(name)
         self.root.protocol("WM_DELETE_WINDOW", self.close)
@@ -36,6 +36,7 @@ class UI(tk.Frame):
     def close(self, event=None):
         result = messagebox.askyesno("Quit", "Are you sure you want to quit?", parent=self)
         if result:
+            Ui.current_open_uis.remove(self)
             self.is_root_destroyed = True
             self.root.destroy()
 
@@ -46,12 +47,12 @@ class UI(tk.Frame):
         def get_icon_file_name(icon_base_name):
             return join(os.path.dirname(os.path.abspath(__file__)), icon_base_name)
 
-        if UI.icon:
+        if Ui.icon:
             return
         try:
             abs_name = str(get_icon_file_name(window_icon))
             photo_image = tk.PhotoImage(file=abs_name)
-            UI.icon = photo_image
+            Ui.icon = photo_image
             self.root.iconphoto(True, photo_image) # Note: has no effect when running in PyCharm IDE
         except Exception as e:
             print("Error loading icon:", e)

3f80d785378075602cd137fcf43b60f7c3be2151
Volker Siegel <mail@volker-siegel.de>
2023-08-07 01:05:09 +0200

    Renamed var

--- thoughttree/Console.py
+++ thoughttree/Console.py
@@ -5,11 +5,11 @@ from tkinter import END
 
 
 class Console(tk.scrolledtext.ScrolledText, io.TextIOBase):
-    def __init__(self, master, width=100, height=5, **kw):
-        tk.scrolledtext.ScrolledText.__init__(self, master, undo=True, wrap=tk.WORD, width=width, height=height,
-            takefocus=False, font=("monospace", 8), **kw)
+    def __init__(self, parent, width=100, height=5, **kw):
+        tk.scrolledtext.ScrolledText.__init__(self, parent, undo=True, wrap=tk.WORD, width=width, height=height,
+                                              takefocus=False, font=("monospace", 8), **kw)
         io.TextIOBase.__init__(self)
-        self.master = master
+        self.parent = parent
         self.vbar.config(width=18, takefocus=False, borderwidth=2)
         self.insert(END, "Console:\n")
 

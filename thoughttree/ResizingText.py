a7f7742e9b6334c74529f572f6fd618665dfac8a
Volker Siegel <mail@volker-siegel.de>
2023-08-10 03:21:09 +0200

    ResizingText changes

--- thoughttree/ResizingText.py
+++ thoughttree/ResizingText.py
@@ -1,28 +1,49 @@
 import tkinter as tk
-from tkinter import ttk
 
 from Notebook import Notebook
+from Sheet import Sheet
 
 
 class ResizingText(tk.Text):
     def __init__(self, parent, wrap="word", highlightthickness=0, borderwidth=0, padx=0, pady=0, *args, **kw):
-        super().__init__(parent, wrap=wrap, highlightthickness=highlightthickness, borderwidth=borderwidth, padx=padx, pady=pady, *args, **kw)
+        super().__init__(parent, wrap=wrap, highlightthickness=highlightthickness, borderwidth=borderwidth, font=Sheet.FONT,
+            padx=padx, pady=pady, *args, **kw)
+        self.old_num_lines = 0
         self.bind("<KeyRelease>", self.adjust_height)
+
+        def yscrollcommand(start, stop):
+            print(f"Scroll command: ")
+            if start != 0.0 or stop != 1.0:
+                self.adjust_height()
+
+        self.configure(yscrollcommand=yscrollcommand)
 
         def on_return(event=None):
             self.insert(tk.INSERT, "\n")
             self.adjust_height()
             return "break"
 
-        self.bind("<Return>", on_return)
-
-        self.old_num_lines = 0
+        # self.bind("<Return>", on_return)
+        self.bind("<Configure>", self.adjust_height)
         self.adjust_height()
+
+    # def on_configure(self, event=None): # todo One Return causes 3 adjust_height() and two on_configure()
+    #     print("on_configure")
+    #     self.adjust_height(event)
 
     def adjust_height(self, event=None):
         num_lines = self.count("1.0", "end", 'displaylines')[0]
-        if num_lines != self.old_num_lines:
+        print(f"adjust_height: {num_lines=}")
+        print(f"adjust_height: {self.winfo_height()=}")
+        print(f"adjust_height: {self.winfo_reqheight()=}")
+
+        height = self.winfo_height()
+        reqheight = self.winfo_reqheight()
+        if num_lines != self.old_num_lines or (height > 1 and height != reqheight):
+            self.see(tk.INSERT)
             self.old_num_lines = num_lines
             self.configure(height=num_lines)
-            if type(self.master) is Notebook:
-                self.master.event_generate("<<NotebookTabChanged>>")
+            print(f"{type(self.master.master)=}")
+            if type(self.master.master) is Notebook:
+                self.master.master.event_generate("<<NotebookTabChanged>>")
+                print(f"<<NotebookTabChanged>>")

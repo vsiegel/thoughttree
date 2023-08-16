6e21b6577d2a109b09ab3fdac413034beb547edc
Volker Siegel <mail@volker-siegel.de>
2023-08-16 22:08:33 +0200

    Make more invisible

--- thoughttree/Notebook.py
+++ thoughttree/Notebook.py
@@ -3,10 +3,12 @@ from tkinter import ttk
 
 
 class Notebook(ttk.Notebook):
-    def __init__(self, parent, style=None, takefocus=False, **kw):
-        if not style:
-            style = "NoBorder.TNotebook"
-            ttk.Style().layout("NoBorder.TNotebook", [])
-        super().__init__(parent, style=style, takefocus=takefocus, **kw)
+    def __init__(self, parent, styleName=None, takefocus=False, **kw):
+        if not styleName:
+            styleName = "NoBorder.TNotebook"
+            style = ttk.Style()
+            style.layout(styleName, [])
+            style.configure(styleName, bd=0, highlightthickness=0, background="white")
+        super().__init__(parent, style=styleName, takefocus=takefocus, **kw)
         self.enable_traversal()
 

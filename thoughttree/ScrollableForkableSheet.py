4f81ae9390ae09e805e241742ed5b3ebbde30fbe
Volker Siegel <mail@volker-siegel.de>
2023-08-16 22:01:06 +0200

    Clean up old code

--- thoughttree/ScrollableForkableSheet.py
+++ thoughttree/ScrollableForkableSheet.py
@@ -28,23 +28,9 @@ class ScrollableForkableSheet(tk.Frame):
 
     def configure_scrollregion(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-        # print(self.canvas.bbox("all"))
-        # print(f"{event.width} x {event.height}")
-
-        # self.canvas.configure(scrollregion=(0, 0, event.width, event.height))
-        # self.canvas.configure(width=event.width, height=event.height)
-        # self.canvas.itemconfigure(self.frame_id, width=event.width)
-        # self.canvas.itemconfigure(self.frame_id, width=event.width, height=event.height)
 
     def configure_width(self, event):
-        print(self.canvas.bbox("all"))
-        print(f"{event.width} x {event.height}")
         self.canvas.itemconfigure(self.frame_id, width=event.width)
-
-        # self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-        # self.canvas.configure(scrollregion=(0, 0, event.width, event.height))
-        # self.canvas.configure(width=event.width, height=event.height)
-        # self.canvas.itemconfigure(self.frame_id, width=event.width, height=event.height)
 
 
 if __name__ == "__main__":

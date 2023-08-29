5356133fca44027f8e5f1e7a388ef31326c21c1e
Volker Siegel <mail@volker-siegel.de>
2023-08-29 15:33:20 +0200

    More log_len

--- thoughttree/History.py
+++ thoughttree/History.py
@@ -1,4 +1,4 @@
-from tools import shorter, logarithmic_length
+from tools import shorter, log_len
 
 
 # class ExtendedList(list):
@@ -11,7 +11,7 @@ from tools import shorter, logarithmic_length
 def log_history_compact(history):
     for message in history:
         text = message['content']
-        print(f"{message['role']}: {shorter(text, 120)} {logarithmic_length(text, 120)}")
+        print(f"{message['role']}: {shorter(text, 120)} {log_len(text, 120)}")
     print()
 
 def history_from_args(system="", message="") :

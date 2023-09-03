88e594b6614bd997793869bbee814bd3d802d81e
Volker Siegel <mail@volker-siegel.de>
2023-09-03 23:02:56 +0200

    Warn on unmatched regions Use "derzeitig" Mark when not found Better title

--- thoughttree/committseparate.py
+++ thoughttree/committseparate.py
@@ -5,25 +5,29 @@ sections = ["Zusammenfassung", "Fehler", "Unstimmigkeiten.", "Argumentationslüc
 
 def replace_group(pattern, input, changes_string):
 
-    m = re.findall(pattern, changes_string)
-    # print(f"{m=}")
-    # print(f"{m.groups()=}")
-
-    for id, title, beschreibung, bisher, vorschlag, suffix in m:
-        print()
+    end_of_previous_match = 0
+    for m in re.finditer(pattern, changes_string):
+        id, title, beschreibung, derzeitig, vorschlag, suffix, lookahead = m.groups()
+        top_section = int(id.split(".")[0])
+        title = title.strip()
+        title = re.sub("^\s*\[.*?\]\s*", "", title)
+        title = f"{sections[top_section]}{title and ': ' or ''}{title}"
         print(f"{id=}")
         print(f"{title=}")
-        # print(f"{beschreibung=}")
-        print(f"{bisher=}")
-        # print(f"{vorschlag=}")
+        print(f"{beschreibung=}")
+        print(f"{derzeitig=}")
+        print(f"{vorschlag[:70]=}")
+        found = input.find(derzeitig)
+        print(f"Found: {found} {found == -1 and '-----------' or ''}")
+        print()
 
-        found = input.find(bisher)
-        print(f"{ found=}")
-        if found > -1:
-            output = input.replace(bisher, vorschlag, 1)
-            open(f"2.6_neuronale_netze-cange-{id}.tex", "w").write(output)
-            open(f"2.6_neuronale_netze-cange-{id}-revert.tex", "w").write(input)
+        if m.start(0) > end_of_previous_match:
+            print(f'Warning: Unmatched text: {m.start(0) - end_of_previous_match} characters: "{changes_string[end_of_previous_match:m.start(0)]}"')
 
+        # if found > -1:
+        #     output = input.replace(derzeitig, vorschlag, 1)
+        #     open(f"2.6_neuronale_netze-cange-{id}.tex", "w").write(output)
+        end_of_previous_match = m.end(0)
 
 input_string = open("/home/siegel/ttt/manuscript/2.6_neuronale_netze.tex").read()
 changes_string = open("/home/siegel/ttt/manuscript/2.6_neuronale_netze_manuscript_changes_one_section-all.txt").read()

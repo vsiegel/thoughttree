1d4daa4f0bbf1b3800c09788f705980a5a69f389
Volker Siegel <mail@volker-siegel.de>
2023-09-16 13:16:01 +0200

    Allow color arg

--- thoughttree/Notebook.py
+++ thoughttree/Notebook.py
@@ -3,12 +3,12 @@ from tkinter import ttk
 
 
 class Notebook(ttk.Notebook):
-    def __init__(self, parent, styleName=None, takefocus=False, **kw):
+    def __init__(self, parent, styleName=None, takefocus=False, background="white", **kw):
         if not styleName:
             styleName = "NoBorder.TNotebook"
             style = ttk.Style()
             style.layout(styleName, [])
-            style.configure(styleName, bd=0, highlightthickness=0, background="white")
+            style.configure(styleName, bd=0, highlightthickness=0, background=background)
         super().__init__(parent, style=styleName, takefocus=takefocus, **kw)
         self.enable_traversal()
         self.winfo_toplevel().unbind('<Control-Tab>')

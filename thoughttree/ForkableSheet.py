b82140f8d2a7a13380471e49a65bff4f3b767949
Volker Siegel <mail@volker-siegel.de>
2023-09-18 23:19:28 +0200

    Many changes

--- thoughttree/ForkableSheet.py
+++ thoughttree/ForkableSheet.py
@@ -1,34 +1,51 @@
 import tkinter as tk
-from tkinter import X, BOTH, Y
+from tkinter import X, BOTH, Y, INSERT
 
 from Notebook import Notebook
 from Sheet import Sheet
 
 
-class ForkableSheet(tk.Frame):
+class ForkableSheet(Sheet):
     def __init__(self, parent, *args, **kw):
-        super().__init__(parent, *args, **kw)
-        self.sheet = Sheet(self, height=1, scrollbar=False, grow=True, background="white",  *args, **kw)
-        self.sheet.pack(side=tk.TOP, fill=BOTH, expand=True, anchor=tk.N)
+        self.fork_frame = tk.Frame(parent)
+        self.fork_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        Sheet.__init__(self,  self.fork_frame, height=1, width=1000, scrollbar=False, grow=True, background="white",  *args, **kw)
+        self.pack(side=tk.TOP, fill=BOTH, expand=True)
 
         self.notebook = None
-        # self.notebook = Notebook(self,)# background="lightgreen")
-        # self.notebook.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=False)
+        self.bind("<Control-o>", self.fork_mock)
 
-        self.sheet.bind("<Control-o>", self.fork_mock)
+
+    def create_notebook(self):
+        self.notebook = Notebook(self.fork_frame)
+        self.notebook.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.pack_configure(expand=False)
+
+    def create_notebook_tab(self, title, text):
+        if not self.notebook:
+            self.create_notebook()
+        frame = tk.Frame(self.notebook)
+        forkable_sheet = ForkableSheet(frame)
+        forkable_sheet.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        if text:
+            forkable_sheet.insert(INSERT, text)
+        self.notebook.add(frame, text=title)
 
 
     def fork_mock(self, event=None):
         if not self.notebook:
-            self.notebook = Notebook(self)
-            self.notebook.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-            self.sheet.pack_configure(expand=False)
+            self.create_notebook()
 
-        frame1 = tk.Frame(self.notebook)
-        text_tab1 = ForkableSheet(frame1)
-        text_tab1.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-        text_tab2 = ForkableSheet(self.notebook)
-        self.notebook.add(frame1, text="1 Tab", sticky=tk.N+tk.E+tk.S+tk.W)
-        self.notebook.add(text_tab2, text="2 Tab", sticky=tk.N+tk.E+tk.S+tk.W)
-        self.notebook.add(ForkableSheet(self.notebook), text="3 Tab")
+        frame = tk.Frame(self.notebook)
+        forkable_sheet = ForkableSheet(frame)
+        forkable_sheet.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.notebook.add(frame, text="Frame Tab")
+
+        frame = tk.Frame(self.notebook)
+        self.notebook.add(frame, text="3 Tab")
+        forkable_sheet = ForkableSheet(frame)
+        forkable_sheet.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
         return "break"
+
+

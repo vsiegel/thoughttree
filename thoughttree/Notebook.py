632adc4055b3659b3c4cc6f1804e94d361eb0bf6
Volker Siegel <mail@volker-siegel.de>
2023-09-23 04:40:52 +0200

    add_sheet

--- thoughttree/Notebook.py
+++ thoughttree/Notebook.py
@@ -1,5 +1,5 @@
+import tkinter as tk
 from tkinter import ttk
-
 
 
 class Notebook(ttk.Notebook):
@@ -12,4 +12,13 @@ class Notebook(ttk.Notebook):
         super().__init__(parent, style=styleName, takefocus=takefocus, **kw)
         self.enable_traversal()
         self.winfo_toplevel().unbind('<Control-Tab>')
+
+    def add_sheet(self, title):
+        frame = tk.Frame(self, name="add_sheet_" + title)
+        from ForkableSheet import ForkableSheet
+        sheet = ForkableSheet(frame)
+        sheet.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.add(frame, text=title)
+        print(f"Added tab: {title}")
+        return sheet
 

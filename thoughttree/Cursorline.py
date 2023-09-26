9c2f0ae5a1a3820e36cbb20cf314bc85d5c46c51
Volker Siegel <mail@volker-siegel.de>
2023-09-26 05:21:35 +0200

    More oo names, directly reference sheet

--- thoughttree/Cursorline.py
+++ thoughttree/Cursorline.py
@@ -3,24 +3,24 @@ class Cursorline:
         self.sheet = sheet
         self.configure_cursorline()
 
-    def cursorline(self, e, add=True):
-        if not e.widget.winfo_exists():
+    def show(self, e=None, add=True):
+        if not self.sheet.winfo_exists():
             return
-        takefocus = not e.widget.cget("takefocus") == "0"
+        takefocus = not self.sheet.cget("takefocus") == "0"
         if not takefocus:
             return
-        e.widget.tag_remove('cursorline', 1.0, "end")
+        self.sheet.tag_remove('cursorline', 1.0, "end")
         if add:
-            e.widget.tag_add('cursorline', 'insert display linestart', 'insert display lineend+1c')
+            self.sheet.tag_add('cursorline', 'insert display linestart', 'insert display lineend+1c')
 
-    def cursorline_remove(self, e):
-        self.cursorline(e, add=False)
+    def clear(self, e=None):
+        self.show(e, add=False)
 
     def configure_cursorline(self):
         self.sheet.tag_config('cursorline', background='#FCFAED', selectbackground="#4682b4", selectforeground="white")
         # self.sheet.tag_config('cursorline', borderwidth=1, relief="solid", selectbackground="#4682b4", selectforeground="white")
         self.sheet.bindtags(self.sheet.bindtags() + ("last",))
-        self.sheet.bind_class("last", '<KeyRelease>', self.cursorline)
-        self.sheet.bind_class("last", '<Button-1>', self.cursorline)
-        self.sheet.bind_class("last", "<FocusIn>", self.cursorline)
-        self.sheet.bind_class("last", "<FocusOut>", self.cursorline_remove)
+        self.sheet.bind_class("last", '<KeyRelease>', self.show)
+        self.sheet.bind_class("last", '<Button-1>', self.show)
+        self.sheet.bind_class("last", "<FocusIn>", self.show)
+        self.sheet.bind_class("last", "<FocusOut>", self.clear)

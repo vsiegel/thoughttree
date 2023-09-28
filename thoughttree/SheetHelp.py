f52a58c2e3e557b394fb37e1c18fb0ca85b723c8
Volker Siegel <mail@volker-siegel.de>
2023-09-28 02:19:35 +0200

    Delete help on first modification

--- thoughttree/SheetHelp.py
+++ thoughttree/SheetHelp.py
@@ -1,18 +1,27 @@
 import tkinter as tk
+from tkinter import INSERT
 
+from Sheet import Sheet
 from Tooltip import Tooltip
 from tools import text_block
 
 
 class SheetHelp(tk.Label):
-    def __init__(self, sheet: tk.Text, text, tooltip=""):
-        super().__init__(sheet, text=text, padx=6, pady=0, bg="white", fg="#b3b3b3", borderwidth=0)
+    def __init__(self, sheet: Sheet, text:str, tooltip=""):
+        super().__init__(sheet, text=text, padx=0, pady=0, bg="white", fg="#b3b3b3", borderwidth=0)
         if tooltip:
             Tooltip(self, text_block(tooltip)).delay_ms = 100
-        sheet.window_create("1.0", window=self)
-        def remove(event):
-            print(f"{event=}")
-            i = sheet.index(self)
-            print(f"{i=}")
-            sheet.delete(self, f"{self}+1c")
+        sheet.window_create("1.2", window=self)
+        # sheet.mark_set(INSERT, f"1.0")
+        def remove(event=None):
+            if sheet.dump('1.0', 'end', window=True):
+                sheet.delete(self, f"{self}+1c")
+
         self.bind("<Button-1>", remove)
+        bind_modified = sheet.bind('<<Modified>>', lambda e: hide(e), add=True)
+        def hide(e=None):
+            print(f"hide: {bind_modified=}")
+            print(f"hide: {sheet.bind('<<Modified>>')=}")
+            remove(e)
+            sheet.unbind('<<Modified>>', bind_modified)
+            print(f"hide: {sheet.bind('<<Modified>>')=}")

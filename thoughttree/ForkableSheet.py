d73cec9d45cbee5199c360b059f145c9aaa5bfdd
Volker Siegel <mail@volker-siegel.de>
2023-09-28 23:35:44 +0200

    Less wide, adding experimental main

--- thoughttree/ForkableSheet.py
+++ thoughttree/ForkableSheet.py
@@ -1,6 +1,8 @@
+import sys
 import tkinter as tk
 from tkinter import X, BOTH, Y, INSERT, END
 
+from IterateRangeForm import IterateRangeForm
 from Notebook import Notebook
 from Sheet import Sheet
 from Title import new_child_title, new_sibling_title, short
@@ -10,7 +12,7 @@ class ForkableSheet(Sheet):
     def __init__(self, parent, *args, **kw):
         self.fork_frame = tk.Frame(parent, name="fork_frame")
         self.fork_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        Sheet.__init__(self,  self.fork_frame, height=1, width=1000, scrollbar=False, grow=True, background="white",  *args, **kw)
+        Sheet.__init__(self,  self.fork_frame, height=1, width=250, scrollbar=False, grow=True, background="white", *args, **kw)
         self.pack(side=tk.TOP, fill=BOTH, expand=True)
         self.notebook = None
 
@@ -20,14 +22,14 @@ class ForkableSheet(Sheet):
             print(f"create_notebook")
             self.notebook = Notebook(self.fork_frame)
             self.notebook.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-            self.pack_configure(expand=False)
+            # self.pack_configure(expand=False)
 
 
     def fork(self, index=INSERT):
         index = self.index(index)
 
         has_leading_text = bool(self.get("1.0", index).strip())
-        parent = self.find_parent(Notebook)
+        parent = self.parent_notebook()
 
         # print(f"{parent=}")
         # print(f"{has_leading_text=}")
@@ -56,3 +58,44 @@ class ForkableSheet(Sheet):
 
 
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    # root.geometry("500x500")
+    root.title(short(__file__))
+    frame = tk.Frame(root)
+    frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    forkable_sheet = ForkableSheet(frame)
+    forkable_sheet.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def grow_to_displaylines(event: tk.Event):
+        if not event.widget.edit_modified():
+            return
+        sheet:ForkableSheet = event.widget
+        ypixels = sheet.count("1.0", "end lineend", 'ypixels')[0]
+
+        if sheet.notebook:
+            height = sheet.notebook.winfo_height()
+            print(f"{height=}")
+            print(f"{sheet.winfo_height()=}")
+            print(f"{ypixels=}")
+            ypixels += sheet.notebook.winfo_height()
+
+        width = sheet.winfo_width()
+
+        frame.pack_propagate(0)
+        frame.configure(height=ypixels, width=width)
+        # root.configure(height=ypixels)
+        # sheet.frame.event_generate("<Configure>", x=1, y=0, width=width, height=ypixels)
+        # sheet.event_generate("<Configure>", x=1, y=0, width=width, height=ypixels)
+        # sheet.update()
+        # sheet.event_generate("<<ScrollRegionChanged>>")
+
+        sheet.edit_modified(False)
+
+    forkable_sheet.bind('<<Modified>>', grow_to_displaylines, add=True)
+    IterateRangeForm(forkable_sheet)
+
+    root.bind('<Escape>', lambda e: sys.exit(0))
+    forkable_sheet.bind("<Control-Alt-f>", lambda e: forkable_sheet.fork())
+    forkable_sheet.focus_set()
+    root.mainloop()

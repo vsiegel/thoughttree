cc2476cda908da08886c58e00634ec0ce6774c65
Volker Siegel <mail@volker-siegel.de>
2023-10-01 15:00:05 +0200

    Subtract borderwidth

--- thoughttree/MultiTextboxLabel.py
+++ thoughttree/MultiTextboxLabel.py
@@ -7,6 +7,7 @@ from Sheet import Sheet
 
 class MultiTextboxLabel(tk.Label):
     def __init__(self, parent=None, sheet=None, **kw):
-        super().__init__(parent, borderwidth=4, anchor=W, wraplength=sheet.winfo_width(),
+        borderwidth = 4
+        super().__init__(parent, borderwidth=borderwidth, anchor=W, wraplength=sheet.winfo_width() - borderwidth * 2,
                          justify=LEFT, font=Fonts.FONT, relief=SUNKEN)
         self.pack(side=TOP, fill=X, expand=True)

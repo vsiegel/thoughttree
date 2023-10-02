38543f13915785a20b8c29231fb6d7618c61da0c
Volker Siegel <mail@volker-siegel.de>
2023-10-02 03:48:28 +0200

    allow using InsertionIcon with "with".

--- thoughttree/InsertionIcon.py
+++ thoughttree/InsertionIcon.py
@@ -1,10 +1,23 @@
 from pathlib import Path
-from tkinter import PhotoImage
+from tkinter import PhotoImage, INSERT
 import tkinter as tk
 
+from Sheet import Sheet
+
+
 class InsertionIcon(tk.Label):
-    def __init__(self, master=None):
+    def __init__(self, master: Sheet, insertion_point=INSERT):
         tk.Label.__init__(self, master)
+        self.sheet = master
+        self.insertion_point = insertion_point
         self.image = PhotoImage(file=Path(__file__).resolve().parent / "chatgpt-24x24.png")
         self.config(image=self.image)
 
+
+    def __enter__(self):
+        self.sheet.mark_gravity(self.insertion_point, tk.LEFT)
+        self.sheet.window_create(self.insertion_point, window=self)
+        self.sheet.mark_gravity(self.insertion_point, tk.RIGHT)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.sheet.delete(str(self), str(self) + " +1c")

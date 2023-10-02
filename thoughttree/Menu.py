a89ab444be5b518843d829bd4f1b977c7218ac08
Volker Siegel <mail@volker-siegel.de>
2023-10-02 03:42:53 +0200

    Remember parent

--- thoughttree/Menu.py
+++ thoughttree/Menu.py
@@ -10,6 +10,7 @@ class Menu(tk.Menu):
 
     def __init__(self, parent: Union[tk.Tk, tk.Text, tk.Menu, None], label=None, menu_help=None, **kw) : #1
         super().__init__(parent, tearoff=False, borderwidth=3, **kw)
+        self.parent = parent
         if menu_help:
             self.menu_help = menu_help
         elif hasattr(parent, "menu_help"):

2578ce56782d3af95c02cb49d342e5becb933190
Volker Siegel <mail@volker-siegel.de>
2023-10-08 08:25:44 +0200

    Renamed tree sheet.

--- thoughttree/TreeSheet.py
+++ thoughttree/TreeSheet.py
@@ -6,7 +6,7 @@ from Sheet import Sheet
 from tools import on_event, bind_tree, iterate_tree
 
 
-class ScrollableForkableSheet(tk.Frame):
+class TreeSheet(tk.Frame):
     def __init__(self, parent, *args, **kw):
         super().__init__(parent, name="sfs", *args, **kw)
 
@@ -28,22 +28,30 @@ class ScrollableForkableSheet(tk.Frame):
 
         def on_mousewheel(event):
             delta = (event.num == 5 and 1 or -1)
+            print(f"on_mousewheel: {event} {delta=} {self.canvas.winfo_containing(event.x_root, event.y_root)} {self.canvas}")
             self.canvas.yview_scroll(delta, "units")
-        self.canvas.bind("<Button-4>", on_mousewheel)
-        self.canvas.bind("<Button-5>", on_mousewheel)
+            str(self.canvas.winfo_containing(event.x_root, event.y_root)).startswith(str(self.canvas))
+        self.winfo_toplevel().bind("<Button-4>", on_mousewheel)
+        self.winfo_toplevel().bind("<Button-5>", on_mousewheel)
 
         self.forkable_sheet.bind("<Configure>", self.configure_scrollregion, add=True)
         self.canvas.bind("<Configure>", self.configure_frame, add=True)
 
     def configure_scrollregion(self, event):
         print(f"configure_scrollregion: {event} {type(event.widget)} {event.widget}")
-        x, y, width, height = bbox = self.canvas.bbox("all")
-        self.canvas.configure(scrollregion=bbox)
+        x, y, width, height = self.canvas.bbox("all")
+        print(f"bbox: {(x, y, width, height)=}")
+
+        self.canvas.configure(scrollregion=(x, y, width, height))
+        print(f"canvas height: {self.canvas.configure('height')}")
+        print(f"cwinfo height: {self.canvas.winfo_height()}")
         self.canvas.event_generate("<Configure>", x=x, y=y, width=width, height=height)
+        # self.canvas.configure(scrollregion=(x, y, width, height), width=width, height=height)
 
     def configure_frame(self, event):
-        print(f"configure_frame:        {event} {type(event.widget)} {event.widget}")
-        height = max(self.canvas.winfo_height(), self.forkable_sheet.winfo_reqheight())
+        print(f"configure_frame:        {event}")
+        # height = max(self.canvas.winfo_height(), self.forkable_sheet.winfo_reqheight())
+        height = self.forkable_sheet.winfo_reqheight()
         self.canvas.itemconfigure(self.canvas_id, width=event.width, height=height)
 
 
@@ -55,7 +63,7 @@ if __name__ == "__main__":
     ui = Ui()
     ui.root.title("ScrollableForkableSheet")
     # ui.root.geometry("500x500")
-    scrollable = ScrollableForkableSheet(ui.root)
+    scrollable = TreeSheet(ui.root)
     scrollable.pack(fill="both", expand=True)
     scrollable.forkable_sheet.focus()
 

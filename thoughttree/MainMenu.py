80199267ed8565ae7c42270a035df7b7b498c698
Volker Siegel <mail@volker-siegel.de>
2023-10-19 00:08:32 +0200

    i to digit

--- thoughttree/MainMenu.py
+++ thoughttree/MainMenu.py
@@ -302,7 +302,7 @@ class MainMenu(Menu):
         self.menu.add_separator()
         sheet_menu.add_separator()
         item("Complete 3 Times", "<Control-Alt-Key-3>", lambda e=None: ui.chat(3), add=True)
-        [self.bind_class("Text", f"<Control-Alt-Key-{i}>", lambda e=None, i=i: ui.chat(i)) for i in [2, 4, 5, 6, 7, 8, 9]]
+        [self.bind_class("Text", f"<Control-Alt-Key-{digit}>", lambda e=None, i=digit: ui.chat(i)) for digit in [2, 4, 5, 6, 7, 8, 9]]
 
         item("Complete Multiple...", "<Control-Shift-M>", lambda e=None: ui.chat(0))
         item("Complete Multiple Again", "<Control-m>", lambda e=None: ui.chat(-1))

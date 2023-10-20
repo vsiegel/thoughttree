f252f08c724de3f675da3f9ae869d8b560bfb2dd
Volker Siegel <mail@volker-siegel.de>
2023-10-20 09:15:45 +0200

    Remember sheet

--- thoughttree/ForkFrame.py
+++ thoughttree/ForkFrame.py
@@ -3,4 +3,5 @@ import tkinter as tk
 
 class ForkFrame(tk.Frame):
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent, name="ff")
+        tk.Frame.__init__(self, parent)
+        self.forkable_sheet = None

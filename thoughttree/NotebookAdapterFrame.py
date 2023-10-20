85220f3c68ff03a288e0cb0b5de8d942272da891
Volker Siegel <mail@volker-siegel.de>
2023-10-20 09:20:28 +0200

    Remove name and warn about it

--- thoughttree/NotebookAdapterFrame.py
+++ thoughttree/NotebookAdapterFrame.py
@@ -3,5 +3,5 @@ import tkinter as tk
 
 class NotebookAdapterFrame(tk.Frame):
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent, name="naf")
+        tk.Frame.__init__(self, parent) # Warning: Naming the frame causes errors (name="...").
         self.forkable_sheet = None

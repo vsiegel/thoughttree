cccdeed7dacc6032b5810d5f1b1d4a05c9e50c68
Volker Siegel <mail@volker-siegel.de>
2023-11-05 18:51:17 +0100

    Shorter name, move adding fixed items and separator

--- thoughttree/ModelsMenu.py
+++ thoughttree/ModelsMenu.py
@@ -25,14 +25,16 @@ class ModelsMenu(Menu):
         #     self.delete(0, present_items - self.fixed_model_menu_items - 1)
         models = self.ui.model.get_available_models()
         models = [model for model in models if "instruct" not in model]
-        for i, model_name in enumerate(models):
+        for i, name in enumerate(models):
             key = None
-            if model_name == "gpt-4":
+            if name == "gpt-4":
                 key = "<Control-Key-4>"
-            elif model_name == "gpt-3.5-turbo":
+            elif name == "gpt-3.5-turbo":
                 key = "<Control-Key-3>"
-            if model_name.find("gpt-3.5") != -1:
-                model_name = "ChatGPT " + model_name
-            command = lambda e=None, model=model_name: self.selected_model.set(model)
-            self.add_item(model_name, key, command)#, variable=self.selected_model, to=self.ui.root)
+            if name.find("gpt-3.5") != -1:
+                name = "ChatGPT " + name
+            command = lambda e=None, model=name: self.selected_model.set(model)
+            self.add_item(name, key, command)#, variable=self.selected_model, to=self.ui.root)
+        self.add_separator()
+        self.add_item("Reload Available Models", None, self.load_available_models)
         return len(models)

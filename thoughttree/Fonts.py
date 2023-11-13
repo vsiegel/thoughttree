61e23816279facbe491ed8386ddc86681228da31
Volker Siegel <mail@volker-siegel.de>
2023-11-13 16:27:50 +0100

    Larger font

--- thoughttree/Fonts.py
+++ thoughttree/Fonts.py
@@ -5,4 +5,4 @@ class Fonts:
     FONT_NAME_PROPORTIONAL = "sans-serif"
     # FONT_NAME_PROPORTIONAL = "DejaVu Sans"
     # FONT_NAME_PROPORTIONAL = "DejaVu Sans Mono ExCond"
-    FONT = (FONT_NAME_PROPORTIONAL, -11) # 11)
+    FONT = (FONT_NAME_PROPORTIONAL, -12) # 11)

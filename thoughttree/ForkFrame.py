bca98d71d5bba358bb8e7a664e2f1c1dedeb6d6c
Volker Siegel <mail@volker-siegel.de>
2023-12-02 03:53:33 +0100

    Frame classes moved to inner classes

--- thoughttree/ForkFrame.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-
-class ForkFrame(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        self.forkable_sheet = None
--- thoughttree/NotebookAdapterFrame.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-
-class NotebookAdapterFrame(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent) # Warning: Naming the frame causes errors (name="...").
-        self.forkable_sheet = None

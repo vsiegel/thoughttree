e8822467129d9cf7a59e4968f31c7a1af2c2b42a
Volker Siegel <mail@volker-siegel.de>
2023-12-11 17:55:39 +0100

    Moved TreeTooltip to separate file

---
+++ thoughttree/TreeTooltip.py
@@ -0,0 +1,35 @@
+from os.path import exists
+
+from Tooltip import Tooltip
+from tree_help_texts import tree_help
+
+
+class TreeTooltip(Tooltip):
+    def __init__(self, tree):
+        self.tree = tree
+        super().__init__(tree, None,("monospace", 9))
+
+    def refresh_tooltip_text(self, event=None):
+        max_lines = 40
+        max_columns = 100
+        iid = self.tree.tree.identify('item', event.x, event.y)
+        text = ""
+        toplevel_node = "Tree." + iid
+        if toplevel_node in tree_help:
+            text = tree_help[toplevel_node].strip()
+        else:
+            values = self.tree.tree.item(iid)["values"]
+            item = values and values[0]
+            if exists(item):
+                with open(item) as f:
+                    text = f.read()
+                    lines = text.splitlines()
+                    lines = lines[:max_lines]
+                    lines = [line[:max_columns] for line in lines]
+                    text = "\n".join(lines)
+        if text.strip():
+            self.label.configure(text=text)
+
+    # def bind_tip(self):
+    #     self.tree.tree.bind("<Leave>", self.hide, add=True)
+    #     self.tree.tree.bind("<Motion>", self.update, add=True)

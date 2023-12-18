6b5d382225246c71073329c4945eced7f23191ff
Volker Siegel <mail@volker-siegel.de>
2023-12-18 13:29:48 +0100

    Gray

--- thoughttree/FinishReasonIcon.py
+++ thoughttree/FinishReasonIcon.py
@@ -5,6 +5,6 @@ from Tooltip import Tooltip
 
 class FinishReasonIcon(tk.Label):
     def __init__(self, parent, symbol: str, tooltip=""):
-        super().__init__(parent, text=symbol, padx=6, pady=0, bg="#F0F0F0", fg="grey", borderwidth=0)
+        super().__init__(parent, text=symbol, padx=6, pady=0, bg="#F0F0F0", fg="gray", borderwidth=0)
         if tooltip:
             Tooltip(self, tooltip).show_delay_ms = 100

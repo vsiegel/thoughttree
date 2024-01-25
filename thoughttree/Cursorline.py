ea3a56aa639a8cb9119f284f32161d16ecb0f02b
Volker Siegel <mail@volker-siegel.de>
2024-01-25 18:51:01 +0100

    Avoid repeating bind_class

--- thoughttree/Cursorline.py
+++ thoughttree/Cursorline.py
@@ -3,10 +3,11 @@ class Cursorline:
         self.sheet = sheet
         self.sheet.tag_config('cursorline', background='#fcfaed', borderwidth=1, relief="raised", selectbackground="#4682b4", selectforeground="white")
         self.sheet.bindtags(self.sheet.bindtags() + ("cursorline_last",))
-        self.sheet.bind_class("cursorline_last", '<KeyRelease>', self.show)
-        self.sheet.bind_class("cursorline_last", '<Button-1>',   self.show)
-        self.sheet.bind_class("cursorline_last", "<FocusIn>",    self.show)
-        self.sheet.bind_class("cursorline_last", "<FocusOut>",   self.clear)
+        if not self.sheet.bind_class("cursorline_last"):
+            self.sheet.bind_class("cursorline_last", '<KeyRelease>', self.show)
+            self.sheet.bind_class("cursorline_last", '<Button-1>',   self.show)
+            self.sheet.bind_class("cursorline_last", "<FocusIn>",    self.show)
+            self.sheet.bind_class("cursorline_last", "<FocusOut>",   self.clear)
 
     def show(self, e=None, add=True):
         if not e.widget.winfo_exists():

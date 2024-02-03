bf3ca1e80db91d323121ec35a2fbeb159e3aed20
Volker Siegel <mail@volker-siegel.de>
2024-02-03 19:49:25 +0100

    New event recorder stub

---
+++ thoughttree/EventRecorder.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk, BOTH, LEFT, RIGHT, VERTICAL, NW, Y, X, INSERT, CURRENT, TOP
+
+import tools
+
+
+class EventRecorder:
+    def __init__(self, w: tk.Widget):
+        self.widget = w
+        self.events = []
+
+        def on_key(event):
+            print(f"{event}")
+            self.events.append(event)
+        self.widget.bind_all("<Key>", self.record)
+
+
+    def record(self, event):
+        if event.type == tk.EventType.KeyPress:
+            print(f"record: <KeyPress-{event.keysym}>", flush=True)
+        else:
+            print(f"record: {event}", flush=True)
+

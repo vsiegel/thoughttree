21578ff4866b584ab1efdf3b7816b61bc2bea2f2
Volker Siegel <mail@volker-siegel.de>
2024-02-04 09:44:37 +0100

    Reproduce events for testing

--- thoughttree/EventPlayer.py
+++ thoughttree/EventPlayer.py
@@ -4,53 +4,37 @@ from tkinter import ttk, BOTH, LEFT, RIGHT, VERTICAL, NW, Y, X, INSERT, CURRENT,
 import tools
 
 
-
 class EventPlayer:
-    def __init__(self, w: tk.Widget):
+    def __init__(self, w: tk.Widget, events=None, start=True):
         self.widget = w
-
-        def lines(n: int) -> str:
-            """:return: str - n lines with the numbers to str 0 to (n-1) with newlines between"""
-            return "\n".join(list(map(str, range(n))))
-
 
         # events = "\n".join([chr(i) for i in range(50, 70)])
         # events = [chr(i) for i in range(65, 85)]
         # events = [4, 5, 6, 7]
         # events = lines(15)
         # events = (["<X>", "<Right>", "<Y>",])
-        events = (["a", "b", "c", "\n", "a", "<b>", "c", "\n", "foo", "bar", 500, "a", "bbbb", ])
+        # events = (["a", "b", "c", "\n", "a", "<b>", "c", "\n", "foo", "bar", 500, "a", "bbbb", ])
                   # + ["<<Undo>>"] * 5 + ["<<Redo>>"] * 5)
-        # events = ["a", "b"]
+        events = events or []
+        self.widget.after_idle(lambda: self.play(events))
 
-        def ttt():
-            w.event_generate("<Key-i>")
-            # widget.event_generate("<Right>")
-            w.update_idletasks()
-            w.update()
-            w.event_generate("<Key-x>")
-
-        # root.after(1000, lambda : self.play(events))
-        # self.widget.bind("<Key>", lambda e: print(e.__dict__), add=True)
         self.widget.bind("<Escape>", lambda e: self.widget.winfo_toplevel().destroy())
-        self.widget.bind("<Control-t>", lambda e: ttt())
         self.widget.bind("<Alt-a>", lambda e: w.event_generate("<a>"))
         self.widget.bind("<Alt-b>", lambda e: w.event_generate("<b>"))
         self.widget.bind("<Control-Alt-Shift-P>", lambda e: self.play(events))
-        # self.widget.bind("<FocusIn>", lambda e: self.play(events))
-        # tools.bind_all_events(self.widget)
+
 
     def generate(self, event):
         w = self.widget
-        print(f"-{w.index(INSERT)} {event=}")
+        # print(f"-{w.index(INSERT)} {event=}")
         w.event_generate(event, state=16, x=0, y=0)
         w.update()
         w.update_idletasks()
-        print(f"+{w.index(INSERT)} {event=}")
+        # print(f"+{w.index(INSERT)} {event=}")
 
 
     def play(self, events, start=0):
-        print(f"play: {start} {events=}", flush=True)
+        # print(f"play: {start} {events=}", flush=True)
         for i, event in enumerate(events):
             if event == "\n":
                 event = "<Return>"
@@ -59,12 +43,9 @@ class EventPlayer:
             elif type(event) is str:
                 self.word(event)
             elif type(event) is int:
-                root.after(event, lambda: self.play(events[i+1:]))
-                return
+                self.widget.after(event, lambda: self.play(events[i+1:]))
             else:
                 self.generate(event)
-            root.after(100, lambda: self.play(events[i+1:]))
-            return
 
 
     def word(self, word: str):

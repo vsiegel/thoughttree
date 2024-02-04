670be90c28052713008db04202278a92658a185b
Volker Siegel <mail@volker-siegel.de>
2024-02-04 09:41:53 +0100

    Ruler to debug tree scrolling

---
+++ thoughttree/Ruler.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+import tools
+from Dragability import Dragability
+
+
+class Ruler(tk.Toplevel):
+
+    def __init__(self, parent, *values_and_labels, offset=0):
+        super().__init__(background="#76838c")
+        self.parent = parent
+        self.offset = offset
+        self.overrideredirect(True)
+        self.geometry("200x1600+2000+0")
+        self.canvas = tk.Canvas(self, width=200, height=1600, background="#a4b8c4")
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        Dragability(self)
+        # self.reset()
+        # self.ticks(*values_and_labels)
+        # def on_button1(event):
+        #     print(event.x, event.y)
+        # self.bind('<Button-1>', on_button1, add=True)
+        # def on_button3(event):
+        #     print(event.x, event.y)
+        # self.bind('<Button-3>', on_button3, add=True)
+
+    def move_home(self):
+        self.geometry("200x1600+2000+0")
+
+    def ticks(self, *values_and_labels):
+        for v, label, *color in values_and_labels:
+            if color:
+                color = color[0]
+            self.tick(v, label, color=color)
+
+    def tick(self, v, label="", color=None):
+        # print(f"{label + ':':15s} {v}")
+        color = color or "black"
+        for i, shift in enumerate([3, 0, 3], -1):
+            self.canvas.create_line(0 + shift, v + self.offset + i, 15, v + self.offset + i, fill=color, tags=("clearable",))
+        self.canvas.create_text(20, v + self.offset - 12, text=str(v) + " " + label, anchor=tk.NW, fill=color, tags=("clearable",))
+
+    def clear(self):
+        self.canvas.delete("clearable")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    # root.wm_withdraw()
+    ruler = Ruler(root, (0, "0"), offset=0)
+
+    tools.escapable(root)
+
+    ruler.ticks((33, "dd"), (666,"sss"), (686,"ssss"))
+    # widget.pack(side=LEFT, fill=BOTH, expand=True)
+    # widget.branch()
+    # widget.notebook.add(TreeSheet(widget.notebook))
+    # widget.pack_propagate(False)
+    ruler.canvas.focus_set()
+    root.mainloop()

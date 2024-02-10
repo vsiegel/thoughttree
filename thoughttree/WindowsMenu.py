de2b79614e98a7da50acabc3737dbd84f575a72a
Volker Siegel <mail@volker-siegel.de>
2024-02-10 11:54:35 +0100

    Repopulate Window-menu

--- thoughttree/WindowsMenu.py
+++ thoughttree/WindowsMenu.py
@@ -12,7 +12,13 @@ class WindowsMenu(TooltipableMenu):
 
     def populate_menu(self):
         # self.delete(0, tk.END)
+        for old_item in self.menu_items:
+            old_item.destroy()
         for open in Ui.current_open_uis:
             item = MenuItem(self.popup, self, open.root.title(), keystroke="", command=lambda e=None, ui=open: ui.toTop())
             item.pack(fill='x')
             self.menu_items.append(item)
+
+    def update_menu(self, event, x=None, y=None):
+        self.populate_menu()
+        super().update_menu(event, x, y)

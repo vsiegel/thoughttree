6a909395ae5dd77178ebb04f7f85e48c2211c5aa
Volker Siegel <mail@volker-siegel.de>
2024-03-17 16:49:45 +0100

    Remove unused class

--- thoughttree/AlternativeSheet.py
+++ thoughttree/AlternativeSheet.py
@@ -5,13 +5,6 @@ from Fonts import Fonts
 from ResizingSheet import ResizingSheet
 
 
-class AlternativeLabel(tk.Label):
-    def __init__(self, parent=None, sheet=None, **kw):
-        borderwidth = 4
-        super().__init__(parent, borderwidth=borderwidth, anchor=W, wraplength=sheet.winfo_width() - borderwidth * 4,
-                         justify=LEFT, font=Fonts.FONT, relief=SUNKEN)
-        self.pack(side=TOP, fill=X, expand=True)
-
 class AlternativeSheet(ResizingSheet):
     def __init__(self, parent=None, borderwidth=0, **kw):
         super().__init__(parent, borderwidth=borderwidth, relief=SUNKEN, **kw)
